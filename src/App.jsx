f74f7ef16d0d391bf6f82926beb7c5bf935b3d90
Henry Chun <chunhyungjoon@gmail.com>
2022-12-27 22:04:13 +1100

    done css for container & showing items

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,57 @@
 import './App.css';
+import { BsFillSunFill, BsFillTrashFill } from 'react-icons/bs';
+import { useState } from 'react';
 
 function App() {
-  return <div className='App'>App</div>;
+  const [todoItems, setTodoItems] = useState([
+    {
+      id: '1234',
+      item: 'Study',
+    },
+    {
+      id: '1235',
+      item: 'Workout',
+    },
+    {
+      id: '1236',
+      item: 'Coding',
+    },
+  ]);
+  return (
+    <div className='container'>
+      <header className='header'>
+        <p className='leftMenu'>
+          <BsFillSunFill />
+        </p>
+        <div className='rightMenu'>
+          <span>All</span>
+          <span>Active</span>
+          <span>Completed</span>
+        </div>
+      </header>
+      <section className='list'>
+        <ul>
+          {todoItems.map((item) => (
+            <li key={item.id} className='item'>
+              <div className='leftList'>
+                <input type='checkbox' className='check' />
+                <span className='item'>{item.item}</span>
+              </div>
+              <button className='trashBtn'>
+                <BsFillTrashFill />
+              </button>
+            </li>
+          ))}
+        </ul>
+      </section>
+      <footer className='footer'>
+        <form className='form'>
+          <input type='text' className='footerInput' />
+          <button className='footerBtn'>Add</button>
+        </form>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
